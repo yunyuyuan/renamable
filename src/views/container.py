d323755ae30e205698017c5fc82a3d52e6f66b18
yunyuyuan <1607439239@qq.com>
2021-01-15 17:54:23 +0800

    gitignore

--- src/views/container.py
+++ src/views/container.py
@@ -9,6 +9,7 @@ class Container(QWidget):
 
         self.frame_head = QFrame(self)
         self.frame_body = QFrame(self)
+        self.frame_footer = QFrame(self)
 
         self.btn_choose_files = Button("批量选择文件", self.frame_head)
         self.btn_choose_folder = Button("导入整个文件夹", self.frame_head)
@@ -17,4 +18,11 @@ class Container(QWidget):
         self.input_regx.setPlaceholderText("输入正则表达式")
         self.do_it = Button("执行操作", self.frame_head)
 
-        self.change_select_all = Button("全选", self.frame_head)
+        self.change_hide_filter_file = Button("隐藏已排除的文件", self.frame_footer)
+        self.change_select_all = Button("全选", self.frame_footer)
+
+        layout = QVBoxLayout()
+        layout.addWidget(self.frame_head)
+        layout.addWidget(self.frame_body)
+        layout.addWidget(self.frame_footer)
+        self.setLayout(layout)
